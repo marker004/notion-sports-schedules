a8ca3fcd562cacc37c59c252fa6e6611501ccab8
Mark Reckard <mark.reckard@gmail.com>
2023-02-16 23:13:17 -0500

    add soccer schedule and tests

--- app/soccer_schedule.py
+++ app/soccer_schedule.py
@@ -1,1 +1,136 @@
-url = "https://www.fotmob.com/api/tvlistings?countryCode=US"
+from shared_items.utils import pp
+from shared_items.interfaces import Notion
+from requests import Response, get
+
+from models.soccer import GameBroadcast, Matches
+from shared import SCHEDULE_DATABASE_ID, NotionSportsScheduleItem
+
+notion = Notion()
+
+schedule_url = "https://www.fotmob.com/api/tvlistings?countryCode=US"
+matches_url = "https://www.fotmob.com/api/matches?date=20230216&timezone=America%2FIndianapolis&ccode3=USA_IN"
+
+schedule_response: Response = get(schedule_url)
+matches_response: Response = get(matches_url)
+
+
+schedule_json: dict = schedule_response.json()
+matches_json: dict = matches_response.json()
+
+
+game_broadcasts: list[GameBroadcast] = [
+    GameBroadcast(**game_broadcast)
+    for sublist in schedule_json.values()
+    for game_broadcast in sublist
+]
+
+matches = Matches.parse_obj(matches_json)
+
+BROADCAST_BADLIST = [
+    "Antenna 20 Years",
+    # 'CBS',
+    # 'CBS Sports Network',
+    # 'CBS Sports Network HD',
+    "CL beIN Sports English SD in Spanish",
+    "ESPN Deportes",
+    "ESPN Deportes HD",
+    # 'ESPN+ USA',
+    "ESPN3",
+    # 'FS2',
+    "Fox Deportes",
+    "Fox Deportes HD",
+    "Fox Soccer Plus",
+    "Fox Soccer Plus HD",
+    "Fox Sports 2 HD",
+    "GOL TV",
+    # 'NBC',
+    "NBC Sports California SAT",
+    "NBC Sports California SAT HD",
+    # 'NBC Sports Chicago',
+    # 'NBC Sports Chicago HD',
+    "NBCSports.com",
+    "Nuestra Tele Internacional",
+    # 'Paramount+',
+    # 'Peacock',
+    "RREC",
+    "SporTV",
+    # 'TNT',
+    # 'TNT HD',
+    # 'TNT HD (Pacific)',
+    "TUDN",
+    "TV5MONDE Etats Unis",
+    "Telemundo Satellite Feed",
+    "Telemundo Satellite Feed Pacific",
+    # 'Telemundo Television Network',
+    "Twitch",
+    "TyC Sports Internacional USA",
+    # 'UNIVERSO',
+    # 'UNIVERSO HD',
+    # 'USA Network',
+    # 'USA Network HD',
+    "UniMas",
+    "UniMas Satellite Feed (Pacific)",
+    "Univision NOW",
+    "Univision Network",
+    "VIX+",
+    "ViX",
+    "Yes Network",
+    "Yes Network HD",
+    "beIN Sports",
+    "beIN Sports En Español",
+    "beIN Sports En Español HD",
+    "beIN Sports HD",
+    "iFollow",
+]
+
+broadcasts = [
+    broadcast
+    for broadcast in game_broadcasts
+    if broadcast.station.name not in BROADCAST_BADLIST and "live" in broadcast.tags
+]
+
+# todo: uniq this list of broadcasts
+
+
+def assemble_matchup(broadcast: GameBroadcast) -> str:
+    teams = sorted(broadcast.program.teams, key=lambda t: t.isHome, reverse=True)
+    team_names = [team.name for team in teams]
+    return " vs ".join(team_names)
+
+
+def format_date(broadcast: GameBroadcast) -> str:
+    return broadcast.startTime.strftime("%Y-%m-%dT%H:%M:%S")
+
+
+def format_network(broadcast: GameBroadcast) -> str:
+    return broadcast.station.name
+
+
+def fetch_league(broadcast: GameBroadcast, matches: Matches) -> str:
+    return next(
+        (league.name for league in matches.leagues if league.id == broadcast.leagueId),
+        "",
+    )
+
+
+def format_sport() -> str:
+    return "⚽"
+
+
+broadcasts.sort(key=lambda b: b.startTime)
+
+schedule_items = [NotionSportsScheduleItem(
+        matchup=assemble_matchup(broadcast),
+        date=format_date(broadcast),
+        network=format_network(broadcast),
+        league=fetch_league(broadcast, matches),
+        sport=format_sport(),
+    ) for broadcast in broadcasts]
+
+for schedule_item in schedule_items:
+    props = notion.assemble_props(schedule_item.format_for_notion_interface())
+
+    # don't do this until deduped and deletion in place
+    # notion.client.pages.create(
+    #     parent={"database_id": SCHEDULE_DATABASE_ID}, properties=props
+    # )
---
+++ tests/test_soccer_schedule.py
@@ -0,0 +1,76 @@
+from models.soccer import GameBroadcast
+from soccer_schedule import League, Matches, assemble_matchup, fetch_league, format_date, format_network, format_sport
+
+from pytest import fixture
+
+@fixture
+def dummy_matches() -> Matches:
+    yield Matches(
+        leagues=[
+            League(id=2, name="League 1"),
+            League(id=879842, name="League 2"),
+            League(id=1, name="League 3")
+        ]
+    )
+
+@fixture()
+def dummy_broadcast_dict() -> dict:
+    yield {
+      "startTime": "/Date(1676821500000)/",
+      "endTime": "/Date(-62135596800000)/",
+      "qualifiers": ["Live"],
+      "station": {
+        "callSign": None,
+        "stationId": "2469_st",
+        "affiliateId": None,
+        "affiliateCallSign": None,
+        "name": "ESPN+ USA",
+        "type": "Stream",
+        "blockedCountryCodes": None
+      },
+      "stationId": "2469_st",
+      "matchId": 3900530,
+      "leagueId": 879842,
+      "parentLeagueId": 57,
+      "program": {
+        "rootId": "3900530",
+        "teams": [
+          { "name": "Ajax", "isHome": True, "teamBrandId": "8593" },
+          { "name": "Sparta Rotterdam", "isHome": False, "teamBrandId": "8614" }
+        ]
+      },
+      "bet365MatchId": 0,
+      "externalId": "225916",
+      "affiliates": [
+        {
+          "langCode": "en",
+          "title": "Watch on ESPN+",
+          "subtitle": "",
+          "link": "https://go.web.plus.espn.com/c/1250707/651866/9070",
+          "callToAction": "Sign Up",
+          "imageUrl": "https://images.fotmob.com/image_resources/upload/ESPNplus.png",
+          "disclaimer": ""
+        }
+      ],
+      "tags": ["live"]
+    }
+
+@fixture()
+def dummy_broadcast(dummy_broadcast_dict) -> GameBroadcast:
+    yield GameBroadcast(**dummy_broadcast_dict)
+
+
+def test_assemble_matchup(dummy_broadcast):
+    assert assemble_matchup(dummy_broadcast) == "Ajax vs Sparta Rotterdam"
+
+def test_format_date(dummy_broadcast):
+    assert format_date(dummy_broadcast) == "2023-02-19T15:45:00"
+
+def test_format_network(dummy_broadcast):
+    assert format_network(dummy_broadcast) == 'ESPN+ USA'
+
+def test_format_sport():
+    assert format_sport() == "⚽"
+
+def test_fetch_league(dummy_broadcast, dummy_matches):
+    assert fetch_league(dummy_broadcast, dummy_matches) == "League 2"
