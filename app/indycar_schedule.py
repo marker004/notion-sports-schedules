9109ba771e82b120ab8965e0395adcd89a2ef28f
Mark Reckard <mark.reckard@gmail.com>
2023-03-07 15:48:54 -0500

    use new NotionScheduler

--- app/indycar_schedule.py
+++ app/indycar_schedule.py
@@ -4,46 +4,36 @@ from bs4 import BeautifulSoup
 from shared_items.interfaces.notion import Notion
 from shared_items.utils import measure_execution
 
-from models.indycar import IndycarResponse
-from shared import ElligibleSportsEnum, clear_db_for_sport, insert_to_database
+from models.indycar import IndycarRace, IndycarResponse
+from shared import ElligibleSportsEnum, NotionScheduler, NotionSportsScheduleItem
 from utils.assemblers import IndycarAssembler
 
 notion = Notion()
 
-indycar_url = "https://www.espn.com/racing/schedule/_/series/indycar"
 
-indycar_response: Response = get(indycar_url)
-
-indycar_html_doc = indycar_response.text
-
-soup = BeautifulSoup(indycar_html_doc, "html.parser")
-
-response = IndycarResponse(
-    race_elements_container=soup.find("table", {"class": "tablehead"})
-)
-
-usable_races = response.usable_races
-
-assembled_items = [
-    IndycarAssembler(race).notion_sports_schedule_item() for race in usable_races
-]
-
-all_props = [
-    notion.assemble_props(schedule_item.format_for_notion_interface())
-    for schedule_item in assembled_items
-]
-
-@measure_execution("deleting existing IndyCar races")
-def delete_existing_races():
-    clear_db_for_sport(ElligibleSportsEnum.INDY_CAR.value)
+@measure_execution("fetching new IndyCar schedule")
+def fetch_schedule_response() -> Response:
+    indycar_url = "https://www.espn.com/racing/schedule/_/series/indycar"
+    return get(indycar_url)
 
 
-delete_existing_races()
+def assemble_usable_games(response: Response) -> list[IndycarRace]:
+    indycar_html_doc = response.text
+
+    soup = BeautifulSoup(indycar_html_doc, "html.parser")
+
+    indycar_response = IndycarResponse(
+        race_elements_container=soup.find("table", {"class": "tablehead"})
+    )
+    return indycar_response.usable_races
 
 
-@measure_execution("inserting IndyCar races")
-def insert_games():
-    insert_to_database(all_props)
+def assemble_notion_items(races: list[IndycarRace]) -> list[NotionSportsScheduleItem]:
+    return [IndycarAssembler(race).notion_sports_schedule_item() for race in races]
 
 
-insert_games()
+response = fetch_schedule_response()
+usable_races = assemble_usable_games(response)
+fresh_items = assemble_notion_items(usable_races)
+
+NotionScheduler(ElligibleSportsEnum.INDY_CAR.value, fresh_items).schedule_them_shits()
--- app/nhl_schedule.py
+++ app/nhl_schedule.py
@@ -1,110 +1,96 @@
 from datetime import datetime
 from urllib import parse
+from requests import Response, get
 
-from models.nhl import LeagueBroadcastSchedule
 from shared_items.utils import pp, measure_execution
 from shared_items.interfaces import Notion
-from requests import Response, get
-from models.nhl_espn import DailyEspnPlusNhlSchedule
 
-from shared import (
-    ElligibleSportsEnum,
-    clear_db_for_sport,
-    insert_to_database,
-)
+from models.nhl import LeagueBroadcastSchedule, Game as LeagueGame
+from models.nhl_espn import DailyEspnPlusNhlSchedule, Event as PowerPlayGame
+from shared import ElligibleSportsEnum, NotionScheduler, NotionSportsScheduleItem
 from utils.assemblers import NhlAssembler, NhlEspnPlusAssembler
 
 notion = Notion()
 
-
 BOOKENDS = ["2022-10-07", "2023-4-13"]
 
-league_schedule_base_url = "https://statsapi.web.nhl.com/api/v1/schedule?"
 
-todays_date = datetime.today().strftime("%Y-%m-%d")
-
-league_schedule_params = (
-    ("startDate", todays_date),
-    ("endDate", BOOKENDS[1]),
-    ("hydrate", "broadcasts(all)"),
-    ("site", "en_nhl"),
-    ("teamId", ""),
-    ("gameType", ""),
-    ("timecode", ""),
-)
-
-assembled_league_schedule_url = league_schedule_base_url + parse.urlencode(
-    league_schedule_params, safe=",()"
-)
-
-power_play_schedule_base_url = (
-    "https://site.web.api.espn.com/apis/v2/scoreboard/header?"
-)
-
-power_play_schedule_params = (
-    ("sport", "hockey"),
-    ("league", "nhl"),
-    ("region", "us"),
-    ("lang", "en"),
-    ("contentorigin", "espn"),
-    ("buyWindow", "1m"),
-    ("showAirings", "buy,live,replay"),
-    ("showZipLookup", "true"),
-    ("tz", "America/Indianapolis"),
-)
-assembled_power_play_schedule_url = power_play_schedule_base_url + parse.urlencode(
-    power_play_schedule_params
-)
-
-power_play_schedule_response: Response = get(assembled_power_play_schedule_url)
-power_play_schedule_json: dict = power_play_schedule_response.json()
+def assemble_league_schedule_url() -> str:
+    league_schedule_base_url = "https://statsapi.web.nhl.com/api/v1/schedule?"
+    todays_date = datetime.today().strftime("%Y-%m-%d")
+    league_schedule_params = (
+        ("startDate", todays_date),
+        ("endDate", BOOKENDS[1]),
+        ("hydrate", "broadcasts(all)"),
+        ("site", "en_nhl"),
+        ("teamId", ""),
+        ("gameType", ""),
+        ("timecode", ""),
+    )
+    return league_schedule_base_url + parse.urlencode(
+        league_schedule_params, safe=",()"
+    )
 
 
-power_play_nhl_schedule = DailyEspnPlusNhlSchedule(**power_play_schedule_json)
-
-usable_power_play_games = power_play_nhl_schedule.usable_games()
-
-assembled_power_play_items = [
-    NhlEspnPlusAssembler(game).notion_sports_schedule_item()
-    for game in usable_power_play_games
-]
-
-schedule_response: Response = get(assembled_league_schedule_url)
-schedule_json: dict = schedule_response.json()
-
-league_broadcast_schedule = LeagueBroadcastSchedule(**schedule_json)
-
-
-usable_games = league_broadcast_schedule.usable_games()
-
-
-assembled_items = [
-    NhlAssembler(game).notion_sports_schedule_item() for game in usable_games
-]
-
-# assuming there's no overlap between national and regional broadcast games
-# and non-blacked out out of market games. can uniq if need be
-combined_games = sorted(
-    assembled_items + assembled_power_play_items, key=lambda x: x.date
-)
-
-all_props = [
-    notion.assemble_props(schedule_item.format_for_notion_interface())
-    for schedule_item in combined_games
-]
+def assemble_power_play_schedule_url() -> str:
+    power_play_schedule_base_url = (
+        "https://site.web.api.espn.com/apis/v2/scoreboard/header?"
+    )
+    power_play_schedule_params = (
+        ("sport", "hockey"),
+        ("league", "nhl"),
+        ("region", "us"),
+        ("lang", "en"),
+        ("contentorigin", "espn"),
+        ("buyWindow", "1m"),
+        ("showAirings", "buy,live,replay"),
+        ("showZipLookup", "true"),
+        ("tz", "America/Indianapolis"),
+    )
+    return power_play_schedule_base_url + parse.urlencode(power_play_schedule_params)
 
 
-@measure_execution("deleting existing NHL games")
-def delete_existing_games():
-    clear_db_for_sport(ElligibleSportsEnum.NHL.value)
+@measure_execution("fetching new NHL schedule")
+def fetch_schedule_json() -> dict:
+    url = assemble_league_schedule_url()
+    schedule_response: Response = get(url)
+    return schedule_response.json()
 
 
-delete_existing_games()
+@measure_execution("fetching new NHL PowerPlay schedule")
+def fetch_power_play_json() -> dict:
+    url = assemble_power_play_schedule_url()
+    power_play_schedule_response: Response = get(url)
+    return power_play_schedule_response.json()
 
 
-@measure_execution("inserting NHL games")
-def insert_games():
-    insert_to_database(all_props)
+def assemble_usable_games() -> list[LeagueGame]:
+    league_broadcast_schedule = LeagueBroadcastSchedule(**schedule_json)
+    return league_broadcast_schedule.usable_games()
 
 
-insert_games()
+def assemble_usable_power_play_games() -> list[PowerPlayGame]:
+    power_play_nhl_schedule = DailyEspnPlusNhlSchedule(**power_play_schedule_json)
+    return power_play_nhl_schedule.usable_games()
+
+
+def assemble_notion_items(
+    league_games: list[LeagueGame], power_play_games: list[PowerPlayGame]
+) -> list[NotionSportsScheduleItem]:
+    assembled_items = [
+        NhlAssembler(game).notion_sports_schedule_item() for game in league_games
+    ]
+    assembled_power_play_items = [
+        NhlEspnPlusAssembler(game).notion_sports_schedule_item()
+        for game in power_play_games
+    ]
+    return sorted(assembled_items + assembled_power_play_items, key=lambda x: x.date)
+
+
+schedule_json = fetch_schedule_json()
+power_play_schedule_json = fetch_power_play_json()
+usable_games = assemble_usable_games()
+usable_power_play_games = assemble_usable_power_play_games()
+combined_items = assemble_notion_items(usable_games, usable_power_play_games)
+
+NotionScheduler(ElligibleSportsEnum.NHL.value, combined_items).schedule_them_shits()
