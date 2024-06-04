755a82daf40a162d5b5a96480cefa28949879eae
mreckard <mark.reckard@gmail.com>
2024-06-04 13:38:33 -0400

    add sports to main

--- app/main.py
+++ app/main.py
@@ -3,27 +3,29 @@
 from datetime import datetime
 
 from f1_schedule import schedule_f1
-# from indycar_schedule import schedule_indycar
+from indycar_schedule import schedule_indycar
 from manual_additions import schedule_manual_events
-# from mlb_schedule import schedule_mlb
+from mlb_schedule import schedule_mlb
 from nba_schedule import schedule_nba
 from nhl_schedule import schedule_nhl
 from soccer_schedule import schedule_soccer
+from ncaa_bball_schedule import schedule_ncaa_bball
 
-# from ncaa_bball_tournament_schedule import schedule_march_madness
+from ncaa_bball_tournament_schedule import schedule_march_madness
 
 format = "%m/%d/%Y, %H:%M:%S"
 
 start = datetime.now()
 print(f"started: {start.strftime(format)}")
 schedule_f1()
-# schedule_indycar()
-# schedule_mlb()
+schedule_indycar()
+schedule_mlb()
 schedule_nba()
 schedule_nhl()
 schedule_soccer()
+schedule_ncaa_bball()
 schedule_manual_events()
-# schedule_march_madness()
+schedule_march_madness()
 
 
 end = datetime.now()
